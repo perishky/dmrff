26a24ca521f6a67b2440b8dd24d2626f76947f8b
Matthew Suderman <matthewsuderman@hotmail.com>
2019-09-13 14:04:00 +0100

    prevent 'large vectors not supported' error

--- R/impute-matrix.r
+++ R/impute-matrix.r
@@ -1,5 +1,14 @@
 impute.matrix <- function(x, FUN=function(x) rowMedians(x, na.rm=T)) {
-    idx <- which(is.na(x), arr.ind=T)
+    ##idx <- which(is.na(x), arr.ind=T)
+    ## the line above causes a 'long vectors not supported yet' error
+    ## when x contains more elements the .Machine$integer.max
+    ## the following 5 lines allows x to be much larger without generating this error
+    idx <- lapply(1:ncol(x), function(i) {
+        idx <- which(is.na(x[,i]))
+        cbind(row=idx, col=rep(i,length(idx)))
+    })
+    idx <- do.call(rbind, idx)
+    
     if (length(idx) > 0) {
         v <- FUN(x)
         v[which(is.na(v))] <- FUN(matrix(v, nrow=1))
@@ -7,3 +16,4 @@ impute.matrix <- function(x, FUN=function(x) rowMedians(x, na.rm=T)) {
     }
     x
 }
+
